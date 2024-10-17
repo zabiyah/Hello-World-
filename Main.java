d165cee0bc7bf5cf01f1be40235baa0884191ce8
zabiyah <zabiyahk@nycstudents.net>
2024-10-17 01:53:04 +0000

    systeeeeeeeeeeeeeeeeeeeeeee

--- Main.java
+++ Main.java
@@ -24,5 +24,11 @@ public class Main
         game.recordPlay(0);
         info = game.getScore();
         System.out.println(info);
+        Scoreboard match = new Scoreboard("Lions", "Tigers");
+        info = match.getScore();
+        System.out.println(info);
+        info = game.getScore();
+        System.out.println(info);
+        
     }
 }
